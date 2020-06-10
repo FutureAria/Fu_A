99abff73414c8ed127fd644921d898a2999dc04a
FutureAria <52123534+FutureAria@users.noreply.github.com>
2020-06-10 09:40:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const Discord = require(discord.js");
 const { CommandoClient } = require('discord.js-commando');
+const ytdl = require('ytdl-core');
+const mance = require("./database/mance.json");
